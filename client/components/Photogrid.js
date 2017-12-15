7b49d22719dc840fa9b78a20a6e5be73181c8acd
Chaz <chazhenricks@gmail.com>
2017-12-15 15:37:35 -0600

    created store.js for redux actions directory and reducers directory

---
+++ client/components/Photogrid.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Photogrid = React.createClass({
+    render(){
+        return(
+            <div className="photo-grid">
+                Im the grid
+            </div>
+        )
+    }
+});
+
+export default Photogrid;
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -7,5 +7,20 @@ import css from './styles/style.styl';
 
 //Import components
 import Main from './components/Main.js';
+import Single from './components/Single.js';
+import Photogrid from './components/Photogrid.js';
 
-render(<Main />, document.getElementById("root"));
+//Import react router deps
+import { Router, Route, IndexRoute, browserHistory} from 'react-router';
+
+const router = (
+    <Router history={browserHistory}>
+        <Route path="/" component={Main}>
+            <IndexRoute component={Photogrid}></IndexRoute>
+            <Route path="/view/:postId" component={Single}></Route>
+        </Route>
+    </Router>
+
+);
+
+render(router, document.getElementById("root"));
