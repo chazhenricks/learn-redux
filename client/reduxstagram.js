7c662020525452d7bc3f96e61d5d9ef7a91760c6
Chaz <chazhenricks@gmail.com>
2017-12-14 18:32:21 -0600

    setup initial component render and main.js to make sure webpack configured correctly

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,11 @@
-// let's go!
+//Import React tooling
+import React from 'react';
+import { render } from 'react-dom';
+
+//Import CSS
+import css from './styles/style.styl';
+
+//Import components
+import Main from './components/Main.js';
+
+render(<Main />, document.getElementById("root"));
